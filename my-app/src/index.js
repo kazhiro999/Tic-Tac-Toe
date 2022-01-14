566378595c597066f0d7401091eec4f1684414a3
kazhiro999 <tobitak@tcd.ie>
2022-01-14 17:44:52 +0900

    changed from Square class to functon component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,38 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button 
-          className = "square" 
-          onClick = {() => this.props.onClick()}
-        >
-          {this.props.value}
-        </button>
-      );
-    }
+
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
   }
   
   class Board extends React.Component {
     constructor(props) {
-        super(props);
-        this.state = {
-          squares: Array(9).fill(null),
-        };
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      };
     }
-
+  
     handleClick(i) {
-        const squares = this.state.squares.slice();
-        squares[i]="x";
-        this.setState({squares: squares});
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
     }
-
+  
     renderSquare(i) {
       return (
-        <Square 
-          value={this.state.squares[i]} 
-          onClick = {() => this.handleClick(i)}
+        <Square
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
         />
       );
     }
@@ -86,3 +82,4 @@ class Square extends React.Component {
     <Game />,
     document.getElementById('root')
   );
+  
